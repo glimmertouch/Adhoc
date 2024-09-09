b0c9fe8cf20100c22292a2f97c869f034d643deb
sher <529926660@qq.com>
2024-09-09 10:00:34 +0800

    80211n

---
+++ 80211n_test.cc
@@ -0,0 +1,322 @@
+#include "ns3/aodv-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/dsdv-module.h"
+#include "ns3/dsr-module.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/olsr-module.h"
+#include "ns3/yans-wifi-helper.h"
+
+#include <random>
+
+#include <fstream>
+#include <iostream>
+
+using namespace ns3;
+using namespace dsr;
+
+NS_LOG_COMPONENT_DEFINE("80211n");
+
+/**
+ * Routing experiment class.
+ *
+ * It handles the creation and run of an experiment.
+ */
+class RoutingExperiment
+{
+  public:
+    RoutingExperiment();
+    /**
+     * Run the experiment.
+     */
+    void Run();
+
+    /**
+     * Handles the command-line parameters.
+     * \param argc The argument count.
+     * \param argv The argument vector.
+     */
+    void CommandSetup(int argc, char** argv);
+
+  private:
+    /**
+     * Setup the receiving socket in a Sink Node.
+     * \param addr The address of the node.
+     * \param node The node pointer.
+     * \return the socket.
+     */
+    Ptr<Socket> SetupPacketReceive(Ipv4Address addr, Ptr<Node> node);
+    /**
+     * Receive a packet.
+     * \param socket The receiving socket.
+     */
+    void ReceivePacket(Ptr<Socket> socket);
+    /**
+     * Compute the throughput.
+     */
+    void CheckThroughput();
+
+    uint32_t port{9};            //!< Receiving port number.
+    uint32_t bytesTotal{0};      //!< Total received bytes.
+    uint32_t packetsReceived{0}; //!< Total received packets.
+
+    std::string m_CSVfileName{"80211n_test.csv"}; //!< CSV filename.
+    int m_nSinks{1};                                      //!< Number of sink nodes.
+    std::string m_protocolName{"OLSR"};                    //!< Protocol name.                                    //!< Tx power.
+    bool m_traceMobility{false};                           //!< Enable mobility tracing.
+    bool m_flowMonitor{false};                             //!< Enable FlowMonitor.
+};
+
+RoutingExperiment::RoutingExperiment()
+{
+}
+
+static inline std::string
+PrintReceivedPacket(Ptr<Socket> socket, Ptr<Packet> packet, Address senderAddress)
+{
+    std::ostringstream oss;
+
+    oss << Simulator::Now().GetSeconds() << " " << socket->GetNode()->GetId();
+
+    if (InetSocketAddress::IsMatchingType(senderAddress))
+    {
+        InetSocketAddress addr = InetSocketAddress::ConvertFrom(senderAddress);
+        oss << " received one packet from " << addr.GetIpv4();
+    }
+    else
+    {
+        oss << " received one packet!";
+    }
+    return oss.str();
+}
+
+void
+RoutingExperiment::ReceivePacket(Ptr<Socket> socket)
+{
+    Ptr<Packet> packet;
+    Address senderAddress;
+    while ((packet = socket->RecvFrom(senderAddress)))
+    {
+        bytesTotal += packet->GetSize();
+        packetsReceived += 1;
+        //NS_LOG_UNCOND(PrintReceivedPacket(socket, packet, senderAddress));
+    }
+}
+
+void
+RoutingExperiment::CheckThroughput()
+{
+    double kbs = (bytesTotal * 8.0) / 1000;
+    bytesTotal = 0;
+
+    std::ofstream out(m_CSVfileName, std::ios::app);
+
+    out << (Simulator::Now()).GetSeconds() << "," << kbs << "," << packetsReceived << ","
+        << m_nSinks << "," << m_protocolName << "" << std::endl;
+
+    out.close();
+    packetsReceived = 0;
+    Simulator::Schedule(Seconds(1.0), &RoutingExperiment::CheckThroughput, this);
+}
+
+Ptr<Socket>
+RoutingExperiment::SetupPacketReceive(Ipv4Address addr, Ptr<Node> node)
+{
+    TypeId tid = TypeId::LookupByName("ns3::UdpSocketFactory");
+    Ptr<Socket> sink = Socket::CreateSocket(node, tid);
+    InetSocketAddress local = InetSocketAddress(addr, port);
+    sink->Bind(local);
+    sink->SetRecvCallback(MakeCallback(&RoutingExperiment::ReceivePacket, this));
+
+    return sink;
+}
+
+void
+RoutingExperiment::CommandSetup(int argc, char** argv)
+{
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("CSVfileName", "The name of the CSV output file name", m_CSVfileName);
+    cmd.AddValue("traceMobility", "Enable mobility tracing", m_traceMobility);
+    cmd.AddValue("protocol", "Routing protocol (OLSR, AODV, DSDV, DSR)", m_protocolName);
+    cmd.AddValue("flowMonitor", "enable FlowMonitor", m_flowMonitor);
+    cmd.AddValue("sinks", "Number of sink nodes", m_nSinks);
+    cmd.Parse(argc, argv);
+
+    std::vector<std::string> allowedProtocols{"OLSR", "AODV", "DSDV", "DSR"};
+
+    if (std::find(std::begin(allowedProtocols), std::end(allowedProtocols), m_protocolName) ==
+        std::end(allowedProtocols))
+    {
+        NS_FATAL_ERROR("No such protocol:" << m_protocolName);
+    }
+}
+
+int
+main(int argc, char* argv[])
+{
+    LogComponentEnable("80211n", LOG_LEVEL_INFO);
+    RoutingExperiment experiment;
+    experiment.CommandSetup(argc, argv);
+    experiment.Run();
+
+    return 0;
+}
+
+void
+RoutingExperiment::Run()
+{
+    Packet::EnablePrinting();
+
+    // blank out the last output file and write the column headers
+    std::ofstream out(m_CSVfileName);
+    out << "SimulationSecond,"
+        << "ReceiveRate,"
+        << "PacketsReceived,"
+        << "NumberOfSinks,"
+        << "RoutingProtocol,"
+         << std::endl;
+    out.close();
+
+    int nWifis = 2;
+
+    double TotalTime = 20.0;
+    std::string rate("2048bps");
+    std::string tr_name("80211n");
+
+
+    Config::SetDefault("ns3::OnOffApplication::PacketSize", StringValue("64"));
+    Config::SetDefault("ns3::OnOffApplication::DataRate", StringValue(rate));
+
+    NodeContainer adhocNodes;
+    adhocNodes.Create(nWifis);
+
+    // setting up wifi phy and channel using helpers
+    WifiHelper wifi;
+    wifi.SetStandard(WIFI_STANDARD_80211n);
+
+    YansWifiPhyHelper wifiPhy;
+    YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default();
+
+    wifiPhy.SetChannel(wifiChannel.Create());
+
+    // Add a mac and disable rate control
+    WifiMacHelper wifiMac;
+
+    wifiMac.SetType("ns3::AdhocWifiMac");
+    NetDeviceContainer adhocDevices = wifi.Install(wifiPhy, wifiMac, adhocNodes);
+
+    MobilityHelper mobilityAdhoc;
+
+    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
+    positionAlloc->Add(Vector(0.0, 0.0, 0.0));
+    positionAlloc->Add(Vector(150.0, 0.0, 0.0));
+    mobilityAdhoc.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobilityAdhoc.Install(adhocNodes);
+
+
+    AodvHelper aodv;
+    OlsrHelper olsr;
+    DsdvHelper dsdv;
+    DsrHelper dsr;
+    DsrMainHelper dsrMain;
+    Ipv4ListRoutingHelper list;
+    InternetStackHelper internet;
+
+    if (m_protocolName == "OLSR")
+    {
+        list.Add(olsr, 100);
+        internet.SetRoutingHelper(list);
+        internet.Install(adhocNodes);
+    }
+    else if (m_protocolName == "AODV")
+    {
+        list.Add(aodv, 100);
+        internet.SetRoutingHelper(list);
+        internet.Install(adhocNodes);
+    }
+    else if (m_protocolName == "DSDV")
+    {
+        list.Add(dsdv, 100);
+        internet.SetRoutingHelper(list);
+        internet.Install(adhocNodes);
+    }
+    else if (m_protocolName == "DSR")
+    {
+        internet.Install(adhocNodes);
+        dsrMain.Install(dsr, adhocNodes);
+        if (m_flowMonitor)
+        {
+            NS_FATAL_ERROR("Error: FlowMonitor does not work with DSR. Terminating.");
+        }
+    }
+    else
+    {
+        NS_FATAL_ERROR("No such protocol:" << m_protocolName);
+    }
+
+    NS_LOG_INFO("assigning ip address");
+
+    Ipv4AddressHelper addressAdhoc;
+    addressAdhoc.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer adhocInterfaces;
+    adhocInterfaces = addressAdhoc.Assign(adhocDevices);
+
+    OnOffHelper onoff1("ns3::UdpSocketFactory", Address());
+    onoff1.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1.0]"));
+    onoff1.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0.0]"));
+
+    for (int i = 0; i < m_nSinks; i++)
+    {
+        Ptr<Socket> sink = SetupPacketReceive(adhocInterfaces.GetAddress(i), adhocNodes.Get(i));
+
+        AddressValue remoteAddress(InetSocketAddress(adhocInterfaces.GetAddress(i), port));
+        onoff1.SetAttribute("Remote", remoteAddress);
+
+        Ptr<UniformRandomVariable> var = CreateObject<UniformRandomVariable>();
+        ApplicationContainer temp = onoff1.Install(adhocNodes.Get(i + m_nSinks));
+        temp.Start(Seconds(var->GetValue(10.0, 11.0)));
+        temp.Stop(Seconds(TotalTime));
+    }
+
+    std::stringstream ss;
+    ss << nWifis;
+    std::string nodes = ss.str();
+
+    std::stringstream ss4;
+    ss4 << rate;
+    std::string sRate = ss4.str();
+
+    // NS_LOG_INFO("Configure Tracing.");
+    // tr_name = tr_name + "_" + m_protocolName +"_" + nodes + "nodes_" + sNodeSpeed + "speed_" +
+    // sNodePause + "pause_" + sRate + "rate";
+
+    // AsciiTraceHelper ascii;
+    // Ptr<OutputStreamWrapper> osw = ascii.CreateFileStream(tr_name + ".tr");
+    // wifiPhy.EnableAsciiAll(osw);
+    //AsciiTraceHelper ascii;
+    //MobilityHelper::EnableAsciiAll(ascii.CreateFileStream(tr_name + ".mob"));
+
+    FlowMonitorHelper flowmonHelper;
+    Ptr<FlowMonitor> flowmon;
+    if (m_flowMonitor)
+    {
+        flowmon = flowmonHelper.InstallAll();
+    }
+
+    NS_LOG_INFO("Run Simulation.");
+
+    CheckThroughput();
+
+    Simulator::Stop(Seconds(TotalTime));
+    Simulator::Run();
+
+    if (m_flowMonitor)
+    {
+        flowmon->SerializeToXmlFile(tr_name + ".flowmon", false, false);
+    }
+
+    Simulator::Destroy();
+}
